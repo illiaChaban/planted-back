23dfa29086f346393494df9214ddf5482222ebfa
Illia Chaban <illia.chaban.8@gmail.com>
2018-05-11 11:16:06 -0400

    should connect to heroku server

--- db.js
+++ db.js
@@ -1,11 +1,29 @@
-const pg = require('pg-promise')();
+// const pg = require('pg-promise')();
 
-const dbConfig = {
-    host: 'localhost',
-    port: 5432,
-    database: 'planted',
-    user: 'illia_chaban',
-};
-const db = pg(dbConfig);
+// const dbConfig = {
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'planted',
+//     user: 'illia_chaban',
+// };
+// const db = pg(dbConfig);
+
+
+const { Client } = require('pg');
+
+const db = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+db.connect();
+
+// db.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
 
 module.exports = db;
